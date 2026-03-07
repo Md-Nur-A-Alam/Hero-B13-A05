0bcf9de51399f218008229cb80562eeca90df216
Md. Nur A Alam <mdnuralam2812@gmail.com>
2026-03-08 05:32:25 +0600

    better optimized and apply ES5 properties for sign in func

--- js/signIn.js
+++ js/signIn.js
@@ -1,15 +1,21 @@
 let sessionToken = false;
-document.getElementById("submit").addEventListener("click", () => {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+const signInFunc = () => {
+    const usernameText = document.getElementById("username");
+    const username = usernameText.value.trim();
+    const passwordText = document.getElementById("password");
+    const password = passwordText.value.trim();
     if (username === "admin" && password === "admin123") {
         sessionToken = true;
+        alert("Login successful!");
         window.location.href = "home.html";
     } else {
-        document.getElementById("alert").innerHTML = `Invalid username or password.` + `<br>` + ` Try default username and password.`;
+        document.getElementById("alert").innerHTML = `**Invalid username or password.**<br>**Try default username and password.**`;
+        usernameText.value = "";
+        passwordText.value = "";
         sessionToken = false;
     }
-});
+
+};
 
 const checkSession = () => {
     if (!sessionToken) {
